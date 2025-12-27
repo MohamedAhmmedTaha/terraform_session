dadd3f0a0e50e436fce0cacc37add2bc19dcdba8
Mohamed Taha <mohamed.a.taha55555@gmail.com>
2025-12-27 18:16:30 +0200

    modify create_file.tf

--- create_file.tf
+++ create_file.tf
@@ -1,4 +1,4 @@
 resource "local_file" "nti_file" {
-  filename = "./NTI_file.txt"
+  filename = "./task1_file.txt" #any name
   content  = "Hello ,I am Mohamed Taha this file created using terraform"
 }
